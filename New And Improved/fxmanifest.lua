c7431584cc727caaf01174e75842f4381cb171b4
itzalfie01 <rohanalfie@gmail.com>
2023-04-14 04:10:55 +0100

    Added Folder Separating the new and improved and the original packs

---
+++ New And Improved/fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'adamant'
+author "Itzalfie01"
+description "A Vehiclepack made for Bjcore Framework with Vehicles from LibertyCity"
+
+game 'gta5'
+
+files {
+    '**/**.meta',
+    '**/**.meta',
+    '**/**.meta',
+    '**/**.meta',
+    '**/**.meta',  
+}
+
+
+data_file 'HANDLING_FILE' '**/**.meta'
+data_file 'VEHICLE_METADATA_FILE' '**/**.meta'
+data_file 'CARCOLS_FILE' '**/**.meta'
+data_file 'VEHICLE_VARIATION_FILE' '**/**.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' '**/**.meta'   -- Not Required
